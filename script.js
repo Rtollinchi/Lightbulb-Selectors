16c3470531e3ab2c45384066d2867ac56c567205
Rtollinchi <rtollinchi23@gmail.com>
2024-09-04 17:14:06 -0400

    finshed lightbulb-selectors

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 // Write your code here
+
+const bulb1 = document.querySelector("#lightbulb1");
+const bulb2 = document.querySelector("#lightbulb2");
+const bulb3 = document.querySelector("#lightbulb3");
+
+const output =  document.querySelector(".subtitle");
+
+let count = 0;
+
+bulb1.addEventListener("click", function() {
+  count++;
+
+  output.innerHTML = `You've clicked the lights
+  ${count} times`;
+
+  console.log(count);
+  bulb1.classList.toggle("active");
+});
+
+bulb2.addEventListener("click", function() {
+  count++;
+
+  output.innerHTML = `You've clicked the lights
+  ${count} times`;
+
+  console.log(count);
+  bulb2.classList.toggle("active");
+});
+
+bulb3.addEventListener("click", function() {
+  count++;
+
+  output.innerHTML = `You've clicked the lights
+  ${count} times`;
+
+  console.log(count);
+  bulb3.classList.toggle("active");
+});
